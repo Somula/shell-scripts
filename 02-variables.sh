4fa1c6a3c52d648140527fd50014b5d754e3a0f5
Somularam <reddysomularamalingeswar>
2023-12-12 15:37:09 +0530

    Variable program

--- 02-variables.sh
+++ 02-variables.sh
@@ -4,5 +4,5 @@ PERSON1=RAMESH
 PERSON2=SURESH
 echo "$PERSON1: Hello $PERSON2, Good Morning"
 echo "$PERSON2: Hello $PERSON1, Good Morning"
-echo "$PERSON1: How are you papa?"
-echo "$PERSON2: I am fine, what about you Nani?"
+echo "$PERSON1: How are you $PERSON2?"
+echo "$PERSON2: I am fine, what about you $PERSON1?"
