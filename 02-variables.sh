07f83a03e7ffa68661e8fdede4fb96fcf5597410
Somularam <reddysomularamalingeswar>
2023-12-12 15:30:11 +0530

    Variable program

--- 02-variables.sh
+++ 02-variables.sh
@@ -2,5 +2,5 @@
 
 echo "Nani: Hello Papa, Good Morning"
 echo "Papa: Hello Nani, Good Morning"
-echo "Nani: How are you papa?
+echo "Nani: How are you papa?"
 echo "Papa: I am fine, what about you Nani?"
