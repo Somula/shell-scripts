ca3177e8df71923d1aecb12104121bde551b90f9
Somularam <reddysomularamalingeswar>
2023-12-13 19:13:24 +0530

    new message

--- 09-install-nginx.sh
+++ 09-install-nginx.sh
@@ -5,8 +5,21 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then
     echo "ERROR: please run the script with root access"
+    exit 1
 else
     echo "Successfully your logged in as root user"
 fi
 
 yum install nginx -y
+
+systemctl enable nginx
+
+systemctl start nginx
+
+rm -rf /usr/share/nginx/html/*
+
+cd /usr/share/nginx/html
+
+curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip
+
+unzip /tmp/web.zip
