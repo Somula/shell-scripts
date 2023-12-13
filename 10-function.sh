b913d09cba6c930f8bebfc8190af8a809363813a
Somularam <reddysomularamalingeswar>
2023-12-13 19:30:32 +0530

    Function

---
+++ 10-function.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DATE=$(date)
+echo "$DATE"
+
+package1=$1
+package2=$2
+
+VALIDATE(){
+    if [ $? -ne 0 ]
+    then
+        echo "ERROR:Inastalling was failed. Please try again"
+    else
+        echo "Successfully installed"
+}
+yum install $ package1 -y
+VALIDATE
+yum install $ package2 -y
+VALIDATE
