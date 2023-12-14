f8109c7d290d208d233304bf27d2d6903e188bca
Somularam <reddysomularamalingeswar>
2023-12-14 14:31:51 +0530

    loops error

--- 11-web-server.sh
+++ 11-web-server.sh
@@ -8,6 +8,8 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
@@ -26,39 +28,39 @@ else
     echo -e "$G Successfully logged in as root user $N"
 fi
 
-yum install nginx -y
+yum install nginx -y &>> $LOGFILE
 
 VALIDATE $? "Installing nginx"
 
-systemctl enable nginx
+systemctl enable nginx &>> $LOGFILE
 
 VALIDATE $? "enable nginx"
 
-systemctl start nginx
+systemctl start nginx &>> $LOGFILE
 
 VALIDATE $? "Started nginx"
 
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>> $LOGFILE
 
 VALIDATE $? "removed the default configuration"
 
-cd /usr/share/nginx/html
+cd /usr/share/nginx/html &>> $LOGFILE
 
 VALIDATE $? "redirected to /usr/share/nginx/html"
 
-curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip
+curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip &>> $LOGFILE
 
 VALIDATE $? "Downloaded the web.zip file"
 
-unzip /tmp/web.zip
+unzip /tmp/web.zip &>> $LOGFILE
 
 VALIDATE $? "unzip the file"
 
-cp /home/centos/shell-scripts/roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp /home/centos/shell-scripts/roboshop.conf /etc/nginx/default.d/roboshop.conf &>> $LOGFILE
 
 VALIDATE $? "created a roboshop.conf file"
 
-systemctl restart nginx
+systemctl restart nginx &>> $LOGFILE
 
 VALIDATE $? "Restarted nginx"
 
