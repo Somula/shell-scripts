9b7242978bd5fc1067fa6bfd73cfabd7740b8091
Somularam <reddysomularamalingeswar>
2023-12-14 12:12:39 +0530

    loops error

--- 10-loops.sh
+++ 10-loops.sh
@@ -2,14 +2,15 @@
 
 ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-R="\e[31m]
-G="\O33[O;32m"
-Y="\O33[O;33m"
-N="\O33[O;0m"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 if [ $ID -ne 0 ]
 then
-    echo "$R ERROR:: Switch to root user $N"
+    echo " $R ERROR:: $N Switch to root user  "
 else
     echo " $G Success $N your root user"
 fi
