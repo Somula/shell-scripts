af2261d129a82f552cb9cc193114731590ce0434
Somularam <reddysomularamalingeswar>
2023-12-14 11:17:22 +0530

    Loops file added

---
+++ 10-loops.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $ID -ne 0 ]
+then
+    echo "ERROR: $R Switch to root user $N"
+else
+    echo " $G Success $N your root user"
+fi
+
