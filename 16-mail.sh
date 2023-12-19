909f6cffb3219027073e5d116756c74713b7fbbb
Somularam <reddysomularamalingeswar>
2023-12-19 13:35:18 +0530

    IFS

--- 16-mail.sh
+++ 16-mail.sh
@@ -3,9 +3,10 @@
 TO_TEAM=$1
 ALERT=$2
 BODY=$3
+ESCAPE_BODY=$(printf '%s\n' "$BODY" | sed -e 's/[]\/$*.^[]/\\&/g');
 TO_ADDRESS=$4
 SUBJECT=$5
-ESCAPE_BODY=$(printf '%s\n' "$BODY" | sed -e 's/[]\/$*.^[]/\\&/g');
+
 
 
 FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT/$ALERT/g" -e "s/BODY/$ESCAPE_BODY/g" template.html)
