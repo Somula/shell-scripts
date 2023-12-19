d1bdbbeb607b910ba978e360e1cd3c713c4e3969
Somularam <reddysomularamalingeswar>
2023-12-19 13:15:05 +0530

    IFS

--- 16-mail.sh
+++ 16-mail.sh
@@ -8,7 +8,7 @@ SUBJECT=$5
 ESCAPE_BODY=$(printf '%s\n' "$BODY" | sed -e 's/[]\/$*.^[]/\\&/g');
 
 
-FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT/$ALERT/g" -e "s/BODY/$BODY/g" template.html)
+FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT/$ALERT/g" -e "s/BODY/$ESCAPE_BODY/g" template.html)
 
 echo "$Final_BODY" | mail -s "$(echo -e "$SUBJECT\nContent-Type: text/html")" "$TO_ADDRESS"
 
