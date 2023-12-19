3eefb00b9ea6354a97492385941c44b6d4e51c94
Somularam <reddysomularamalingeswar>
2023-12-19 12:01:08 +0530

    IFS

--- 15-disk-usage.sh
+++ 15-disk-usage.sh
@@ -6,16 +6,17 @@ N="\e[0m"
 
 DISK_USAGE=$(df -hT | grep -vE 'tmp|File')
 DISK_Threshold=1
+message=""
 
-while IFS= read -r line
+while IFS= read line
 do
     USAGE=$(echo $line | awk '{print $6F)' | cut -d % -f1)
     partion=$(echo $line | awk '{print $1F}')
     if [ $USAGE -gt DISK_Threshold ]
     then
-        message+="Higher disk usage on $partion $R$USAGE$N.\n"
+        message+="Higher disk usage on $partion $USAGE.\n"
     fi
 
-    echo -e "Message: $message"
-
 done <<< $DISK_USAGE
+
+echo -e "Message: $message"
