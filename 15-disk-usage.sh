0f83a4a550d7028ee3b59f5853c6b6e0935fb68f
Somularam <reddysomularamalingeswar>
2023-12-19 13:38:10 +0530

    IFS

--- 15-disk-usage.sh
+++ 15-disk-usage.sh
@@ -22,9 +22,9 @@ done <<< $DISK_USAGE
 echo -e "Message: $message"
 
 
-echo "$message" | mail -s "High Disk Usage" somulalingareddy2@gmail.com
+#echo "$message" | mail -s "High Disk Usage" somulalingareddy2@gmail.com
 
-#sh 16-mail.sh "DevOps Team" "HIGH DISK USAGE" "$message" "somulalingareddy2@gmail.com" "Alert High Disk Usage"
+sh 16-mail.sh "DevOps Team" "HIGH DISK USAGE" "$message" "somulalingareddy2@gmail.com" "Alert High Disk Usage"
 
 
 
