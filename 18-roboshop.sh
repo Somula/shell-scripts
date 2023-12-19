32b610ea5964874f1701550af5c37cb3725a1568
Somularam <reddysomularamalingeswar>
2023-12-19 23:18:31 +0530

    Automation of instance script

--- 18-roboshop.sh
+++ 18-roboshop.sh
@@ -5,6 +5,6 @@ SG="sg-0a4b59538c2adbf03"
 INSTANCE=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 
 for i in "${INSTANCE[@]}"
-then
+do
     aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SG
-fi
+done
