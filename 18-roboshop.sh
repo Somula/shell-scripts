5c7ecd59dccbc78ac5b1d265afb17cc0556578b4
Somularam <reddysomularamalingeswar>
2023-12-20 00:14:52 +0530

    Automation of instance script

--- 18-roboshop.sh
+++ 18-roboshop.sh
@@ -3,6 +3,11 @@
 AMI_ID="ami-03265a0778a880afb"
 SG="sg-0a4b59538c2adbf03"
 INSTANCE=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+zoneid="Z0790319IEVF09XXXVM1"
+DOMAIN_NAME=".lingaaws.tech"
+
+
+
 
 for i in "${INSTANCE[@]}"
 do
@@ -13,5 +18,24 @@ do
     else
         INSTANCE_TYPE="t2.micro"
     fi
-    aws ec2 run-instances --image-id $AMI_ID --instance-type $INSTANCE_TYPE --security-group-ids $SG --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+    PrivateIpAddress=$(aws ec2 run-instances --image-id $AMI_ID --instance-type $INSTANCE_TYPE --security-group-ids $SG --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query Instances[0].PrivateIpAddress --output text)
+
+    echo "$i: $PrivateIpAddress"
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $zoneid \
+    --change-batch '
+    {
+        "Comment": "Creating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "CREATE"
+        ,"ResourceRecordSet"  : {
+            "Name"              : ""$i""$DOMAIN_NAME""
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "$PrivateIpAddress"
+            }]
+        }
+        }]
+    }'
 done
